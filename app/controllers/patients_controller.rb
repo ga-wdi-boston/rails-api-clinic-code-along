6c48377169d8713f45dd0cdfb7b54cb443997a18
payne-chris-r <06cpayne@gmail.com>
2017-07-13 17:21:39 -0400

    Adds destroy for patients
    Adds destroy action for PatientsController allowing a DELETE to
    '/patients/:id' to delete a single patient. Also adds a 'set_patient'
    method to be run before either show or destroy in order to keep our code
    DRY.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 class PatientsController < ApplicationController
+  before_action :set_patient, only: [:show, :destroy]
+
   # GET /patients
   # GET /patients.json
   def index
@@ -8,8 +12,18 @@ class PatientsController < ApplicationController
   end
 
   def show
-    @patient = Patient.find(params[:id])
-
     render json: @patient
+  end
+
+  def destroy
+    @patient.destroy
+
+    head :no_content
+  end
+
+  private
+
+  def set_patient
+    @patient = Patient.find(params[:id])
   end
 end

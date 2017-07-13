5204533fe5f2c856549cd7bcb5f0a243bef26c91
payne-chris-r <06cpayne@gmail.com>
2017-07-13 17:25:59 -0400

    Adds update functionality for patients
    Adds the update method in PatientsController so that we can run a
    PATCH (or PUT) to `/patients/:id` and update a patient. Also adds
    `patient_params` method to the PatientsController class so that
    we can filter/protect what types of attributes can be set.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PatientsController < ApplicationController
-  before_action :set_patient, only: [:show, :destroy]
+  before_action :set_patient, only: [:show, :update, :destroy]
 
   # GET /patients
   # GET /patients.json
@@ -15,6 +15,14 @@ class PatientsController < ApplicationController
     render json: @patient
   end
 
+  def update
+    if @patient.update(patient_params)
+      head :no_content
+    else
+      render json: @patient.errors, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @patient.destroy
 
@@ -25,5 +33,9 @@ class PatientsController < ApplicationController
 
   def set_patient
     @patient = Patient.find(params[:id])
+  end
+
+  def patient_params
+    params.require(:patient).permit(:born_on, :diagnosis, :name)
   end
 end
